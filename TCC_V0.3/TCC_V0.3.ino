696f012633840c8c6efb2c1533d4b1e9e94614e9
HenriqueSN <36803644+HenriqueSN@users.noreply.github.com>
2020-10-15 13:29:40 -0300

    TAmbiente+HAmbiente+TSolo+Luminosidade
    Temperatura ambiente, umidade ambiente e temperatura solo sendo captados corretamente

---
+++ TCC_V0.3/TCC_V0.3.ino
@@ -0,0 +1,77 @@
+//Funciona Sensor de humidade, temperatura e temperatura do solo
+//Inclui bibliotecas
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+//Seta pin do DHT
+#define DHTPIN 4
+
+//Seta pin DS18B20
+const int DS18B20PIN = 5; 
+
+//Seta pin LDR
+const int LDRPIN = 34;
+
+//Variável para gravar o valor do LDR
+int ldrValue = 0;
+
+//Seleciona tipo do DHT
+#define DHTTYPE DHT11
+
+//Inicia sensor DHT
+DHT dht(DHTPIN, DHTTYPE);
+
+//Inicia o DS18B20
+OneWire oneWire(DS18B20PIN);
+
+//passa a referencia do onewire para a biblioteca dallas
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  sensors.begin();
+}
+
+void loop() {
+
+  //lê os valores a cada 1 segundo
+  delay(1000);
+
+   
+  //lê humidade
+  float h = dht.readHumidity();
+  //lê temperatura
+  float t = dht.readTemperature();
+
+  //lê temperatura do solo
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+
+  //lê o valor de LDR
+   ldrValue = analogRead(LDRPIN);
+
+  //verifica se a leitura do DHT funciona, caso não, dá um erro
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Falha ao ler sensor DHT"));
+    return;
+  }
+
+
+
+  Serial.print(F("Humidade: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperatura: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(" Temperatura do solo: ");
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.println(" Luminosidade:");
+  Serial.println(ldrValue);
+  Serial.println();
+ 
+
+}
